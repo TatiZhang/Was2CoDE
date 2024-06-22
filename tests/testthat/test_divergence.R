5974c27764f4d8000e3481c953f610d1b44277a6
TatiZhang <Wenjiz1@uw.edu>
2024-06-22 16:32:51 -0700

    testing shape term by coding the quantile-correlation

--- tests/testthat/test_divergence.R
+++ tests/testthat/test_divergence.R
@@ -1,11 +1,11 @@
 context("Test divergence")
 library(testthat)
 library(transport) 
-
+library(Rcpp) 
 
 test_that("divergence function calculates correct values", {
   a <- c(1, 2, 3, 4, 5)
-  b <- c(5, 4, 3, 2, 1)
+  b <- c(1, 4, 3, 2, 1)
   
   result <- divergence(a, b)
   
@@ -40,4 +40,35 @@ test_that("divergence function handles incorrect input types", {
   a <- "not numeric"
   b <- "not numeric"
   expect_error(divergence(a, b))
+})
+
+
+test_that("manual shape calculation is correct", {
+  set.seed(24)
+  a <- rnorm(100,mean=0,sd=1)
+  b <- rnorm(100,mean=2,sd=1)
+  
+  quantiles <- seq(0.1, 0.9, by = 0.1)
+  quantiles_a <- quantile(a, probs = quantiles)
+  quantiles_b <- quantile(b, probs = quantiles)
+  quantile_cor_ab <- cor(quantiles_a, quantiles_b)
+  
+  mean_a <- mean(a)
+  mean_b <- mean(b)
+  sd_a <- sd(a)
+  sd_b <- sd(b)
+  
+  cat("Intermediate values:\n")
+  cat("mean_a:", mean_a, "\n")
+  cat("mean_b:", mean_b, "\n")
+  cat("sd_a:", sd_a, "\n")
+  cat("sd_b:", sd_b, "\n")
+  cat("quantile_cor_ab:", quantile_cor_ab, "\n")
+  
+  shape_manual 		= abs(2 * sd_a * sd_b * (1 - quantile_cor_ab))
+
+  
+  result <- divergence(a, b)
+  
+  expect_equal(result[6], shape_manual)
 })
