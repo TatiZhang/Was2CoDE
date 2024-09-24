3ab79d454fb9979e16977a6316f367d33011471b
TatiZhang <Wenjiz1@uw.edu>
2024-09-24 15:25:40 -0700

    pushing simulation

--- R/simulation.R
+++ R/simulation.R
@@ -3,18 +3,6 @@ set.seed(10)
 
 n <- 10
 
-# Color palettes for case and control groups
-case_color_palette <- grDevices::colorRampPalette(c(rgb(140, 0, 0, maxColorValue = 255),
-                                                    rgb(244, 84, 84, maxColorValue = 255)))(n)
-control_color_palette <- grDevices::colorRampPalette(c(rgb(47, 60, 190, maxColorValue = 255),
-                                                       rgb(27, 198, 245, maxColorValue = 255)))(n)
-transparent_gray <- rgb(0.5, 0.5, 0.5, 0.3)
-two_letters <- substr(transparent_gray, start = 8, stop = 9)
-
-# Add transparency to the palettes
-case_color_trans_palette <- paste0(case_color_palette, two_letters)
-control_color_trans_palette <- paste0(control_color_palette, two_letters)
-
 # Mean and standard deviation vectors for cases and controls
 case_mean_vec <- 2 + runif(n, min = -0.5, 0.5)  # Shifted means for case group
 case_sd_vec <- rep(1, n)  # Standard deviation for case group
@@ -49,41 +37,6 @@ control_gaussian_list <- lapply(1:n, function(i) {
   yseq <- yseq / max(yseq) * 4 * control_size_vec[i]  # Normalize and scale
   cbind(xseq, yseq)  # Return x and y values as a matrix
 })
-
-# Create the plot
-par(mar = c(3, 0.25, 0, 0.25))
-plot(NA,
-     xlim = xlim,
-     ylim = ylim,
-     xaxt = "n", yaxt = "n", bty = "n",
-     xlab = "", ylab = "")
-
-# Add control group polygons to the plot
-for(i in 1:n) {
-  graphics::polygon(x = c(control_gaussian_list[[i]][,1], rev(control_gaussian_list[[i]][,1])),
-                    y = 2 + c(control_gaussian_list[[i]][,2], rep(0, nrow(control_gaussian_list[[i]]))),
-                    col = control_color_trans_palette[i])
-}
-
-# Add case group polygons to the plot
-for(i in 1:n) {
-  graphics::polygon(x = c(case_gaussian_list[[i]][,1], rev(case_gaussian_list[[i]][,1])),
-                    y = c(case_gaussian_list[[i]][,2], rep(0, nrow(case_gaussian_list[[i]]))),
-                    col = case_color_trans_palette[i])
-}
-
-# Add rug plots to indicate the means
-for(i in 1:n) {
-  graphics::rug(control_mean_vec[i],
-                col = control_color_palette[i],
-                lwd = 2)
-  graphics::rug(case_mean_vec[i],
-                col = case_color_palette[i],
-                lwd = 2)
-}
-
-# Add the x-axis
-axis(side = 1, lwd = 2, cex.axis = 1.25, cex.lab = 1.25)
 
 n_cases <- 10
 n_controls <- 10
