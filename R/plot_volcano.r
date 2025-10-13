4c739af03a95ff86d8997eab40236d1cbd131a31
linnykos <kzlin@uw.edu>
2025-10-13 15:41:01 -0700

    Fixing bad typo

--- R/plot_volcano.r
+++ R/plot_volcano.r
@@ -6,13 +6,13 @@ plot_volcano <- function(df,
                          xlim_quantile = c(0.005, 0.995)){
   stopifnot(length(rownames(df)) == nrow(df))
   
-  pCutoff <- max(de_res$p_val[which(de_res$p_val_adj <= padj_cutoff)])
-  FCcutoff <- stats::quantile(abs(de_res$avg_log2FC), probs = FCcutoff_quantile)
-  xlim <- stats::quantile(de_res$avg_log2FC, probs = xlim_quantile)
+  pCutoff <- max(df$p_val[which(df$p_val_adj <= padj_cutoff)])
+  FCcutoff <- stats::quantile(abs(df$avg_log2FC), probs = FCcutoff_quantile)
+  xlim <- stats::quantile(df$avg_log2FC, probs = xlim_quantile)
   
   plot1 <- EnhancedVolcano::EnhancedVolcano(
-    de_res,
-    lab = rownames(de_res),
+    df,
+    lab = rownames(df),
     x = logFC_variable,
     y = pvalue_variable,
     xlim = xlim,
